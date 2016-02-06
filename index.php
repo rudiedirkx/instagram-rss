da115d20bb7418372ee67e97e28f38f6828d248f
Rudie Dirkx <github@hotblocks.nl>
2016-02-06 02:07:30 +0100

    image & link+

--- index.php
+++ index.php
@@ -19,6 +19,9 @@ $json = trim($match[1], ' ;');
 $data = json_decode($json, true);
 $media = $data['entry_data']['ProfilePage'][0]['user']['media']['nodes'];
 
+// print_r($media);
+// exit;
+
 // 3. Print RSS
 echo '<?xml version="1.0" encoding="utf-8"?>' . "\n";
 
@@ -28,19 +31,21 @@ echo '<?xml version="1.0" encoding="utf-8"?>' . "\n";
 		<title>@<?= html($username) ?></title>
 		<link>https://www.instagram.com/<?= html($username) ?>/</link>
 		<description>@<?= html($username) ?></description>
-
-		<? foreach ($media as $node): ?>
+		<? foreach ($media as $node):
+			$link = $node['display_src'];
+			$thumb = $node['thumbnail_src'];
+			$title = trim(trim(@$node['caption']) . ' -- https://www.instagram.com/p/' . $node['code'] . '/');
+			?>
 			<item>
-				<title><?= html(trim($node['caption'])) ?></title>
-				<link>https://www.instagram.com/p/<?= html($node['code']) ?>/?taken-by=<?= html($username) ?></link>
+				<title><?= html($title) ?></title>
+				<link><?= html($link) ?></link>
 				<image>
-					<url><?= html($node['display_src']) ?></url>
-					<link>https://www.instagram.com/p/<?= html($node['code']) ?>/?taken-by=<?= html($username) ?></link>
-					<title><?= html(trim($node['caption'])) ?></title>
+					<url><?= html($thumb) ?></url>
+					<link><?= html($link) ?></link>
+					<title><?= html($title) ?></title>
 				</image>
-
 				<guid isPermaLink="true">https://www.instagram.com/p/<?= html($node['code']) ?>/</guid>
-				<description><?= html(trim($node['caption'])) ?></description>
+				<description><?= html($title) ?></description>
 				<pubDate><?= date('r', $node['date']) ?></pubDate>
 				<author><?= html($username) ?>@instagram.com</author>
 			</item>
